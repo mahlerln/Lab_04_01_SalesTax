d3cc77dfeaa7cbe966de60c0552d523bff916398
Logan <mahlerln@mail.uc.edu>
2025-01-31 22:38:21 -0500

    Final for task 1

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
 
        
 
         System.out.print("What is the price of your purchase: ");
-   double basePrice = scanner.nextDouble();
+   double basePrice = 50;
 
 double salesTax = 0.05 * basePrice;
 
         System.out.printf("The price is $" + basePrice);
 System.out.printf(" The price of tax is $" +  salesTax);
 System.out.printf(" The total price is $" + (basePrice + salesTax));
-scanner.close();
-
-
-
-
 
 
 
